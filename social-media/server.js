1e829551de22d88dac579210600844e0586e73bc
Sameer Kushwaha <sameerkushwaha2003@gmail.com>
2025-04-18 11:47:27 +0530

    i have call the apis the send the reresponse

--- social-media/server.js
+++ social-media/server.js
@@ -4,14 +4,31 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
 
 // Define a route for the root URL
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/evaluation-services/users", async (req, res) => {
+  const response = await fetch('https://20.244.56.144/evaluation-services/users');
+  const data = await response.json();
+  res.send(data);
+});
 
+app.get('/evaluation-services/users/:userId/posts', (req,res)=>{
+  const response = fetch(`https://20.244.56.144/evaluation-services/users/${req.params.userId}/posts`);
+  const data = response.json();
+  res.send(data);
 })
 
-const PORT=  5000;
+app.get('/evaluations-services/posts:postId/comments', async(req,res)=>{
+  const response = await fetch(`https://20.244.56.144/evaluation-services/posts/${req.params.postId}/comments`);
+  const data = await response.json();
+  res.send(data);
+})
+
+
+const PORT= process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${PORT}`);
 })
